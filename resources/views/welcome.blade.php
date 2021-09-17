98d4f478a08b3fabce3c7ee0a4f4cb0c1a5c9b29
JAO <soriano1820207@mkt.ceu.edu.ph>
2021-09-17 21:56:38 +0800

    Paynals

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PAYNALS</title>
+  <link rel="stylesheet" href="css/style.css">
+
+  <script src="{{ asset('js/app.js') }}" defer></script>
+
+
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-white">
+    <div class="content border border-dark bg-opacity-tan p-5 my-5 mx-4">
+      <div class="user-input text-center bg-opacity-secondary p-3">
+        <form action="/" method="get">
+          @csrf
+          <div class="row m-2">
+            <div class="col-sm-6"><label for="value1">City</label></div>
+            <div class="col-sm-6"><label for="value1">Language</label></div>
+          </div>
+          <div class="row m-2">
+            <div class="col-sm-6"><input type="text" name="q" id="" placeholder="Input City" value="Manila"></div>
+            <div class="col-sm-6"><input type="text" name="language" id="" placeholder="en-us" value="en-us"></div>
+          </div>
+
+          <button type="submit" class="mt-3 btn btn-secondary">Search</button>
+        </form>
+      </div>
+    </div>
+
+    <div class="border border-dark bg-opacity-tan p-5 my-5 mx-4">
+      <div class="results text-center bg-opacity-secondary p-3">
+        <div class="row">
+          <div class="col-sm-12"><h3>Search Results</h3></div>
+        </div>
+        <div class="row border-dark m-4">
+          @isset($data)
+
+          <div class="col-3"><h4>Country</h4></div>
+          <div class="col-9">
+            {{print_r($data[0]["Country"]["LocalizedName"])}}
+          </div>
+
+          </div>
+          @endisset
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
